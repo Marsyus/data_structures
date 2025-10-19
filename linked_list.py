93c6c0496ce398961f35633397639dc313d9856c
Marsyus <miadolomboy0917@gmail.com>
2025-10-19 23:27:12 +0800

    Fixed new_node pointing to itself in insert_at_beginning

--- linked_list.py
+++ linked_list.py
@@ -8,7 +8,8 @@ class LinkedList:
     def insert_at_beginning(self, data):
         new_node = Node(data)
         if self.head:
-            new_node.next = new_node
+            new_node.next = self.head
+            self.head = new_node
         else:
             self.head = new_node
             self.tail = new_node
